4e9ad276b39fe943da39b38cc32af6d4e82f2e78
joshr <joshr>
2026-03-03 15:22:16 -0500

    fix debian install script

--- utils/deb/install.sh
+++ utils/deb/install.sh
@@ -1,6 +1,15 @@
-sudo sh -c "echo 'deb https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main' >> /etc/apt/sources.list"
-wget -qO - 'https://gitsecret.jfrog.io/artifactory/api/gpg/key/public' | sudo apt-key add -
-sudo apt-get update && sudo apt-get install -y git-secret
+set -eu
+
+readonly KEYRING_PATH='/usr/share/keyrings/git-secret-archive-keyring.gpg'
+
+wget -qO - 'https://gitsecret.jfrog.io/artifactory/api/gpg/key/public' \
+  | gpg --dearmor \
+  | sudo tee "${KEYRING_PATH}" > /dev/null
+
+sudo sh -c "echo 'deb [signed-by=${KEYRING_PATH}] https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main' > /etc/apt/sources.list.d/git-secret.list"
+
+sudo apt-get update
+sudo apt-get install -y git-secret git
 
 # Testing, that it worked:
 git secret --version
